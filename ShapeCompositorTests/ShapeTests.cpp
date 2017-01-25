af8ce5fe39ce466f464f7e55c42fd93bc03a2ee0
7kia <il7kia@yandex.ru>
2017-01-25 18:06:04 +0300

    Add test project, tests for CShape

---
+++ ShapeCompositorTests/ShapeTests.cpp
@@ -0,0 +1,38 @@
+// Lab2-1Test.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "Canvas/Shapes/CShape.h"
+
+BOOST_AUTO_TEST_SUITE(CShape_suite)
+
+struct Shape_
+{
+	CShape shape;
+};
+BOOST_FIXTURE_TEST_SUITE(Shape, Shape_)
+	BOOST_AUTO_TEST_CASE(Have_position)
+	{
+		Vec2f pos(5.f, 10.f);
+
+		shape.SetPosition(pos); 
+
+		BOOST_CHECK(shape.GetPosition() == pos);
+	}
+	BOOST_AUTO_TEST_CASE(Have_inner_color)
+	{
+		Color color(0.2f, 0.4f, 0.1f);
+		shape.SetInnerColor(color);
+
+		BOOST_CHECK(shape.GetInnerColor() == color);
+	}
+	BOOST_AUTO_TEST_CASE(Have_outer_color)
+	{
+		Color color(0.3f, 0.5f, 0.13f);
+		shape.SetOuterColor(color);
+
+		BOOST_CHECK(shape.GetOuterColor() == color);
+	}
+BOOST_AUTO_TEST_SUITE_END()// Shape_
+
+BOOST_AUTO_TEST_SUITE_END()// CShape
---
+++ ShapeCompositorTests/stdafx.h
@@ -0,0 +1,29 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// For D2D
+/*
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+#include <afxwin.h>
+
+*/
+
+#define BOOST_TEST_MODULE ProcessVectorTestModule
+#define BOOST_TEST_INCLUDED
+#pragma warning (disable:4702)
+#include <boost/test/unit_test.hpp>
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
